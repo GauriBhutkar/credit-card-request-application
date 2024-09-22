21039f357d50fe3d4ead5717700f582af709fd72
Gauri Manoj Bhutkar <g.bhutkar@ALHQLAP-794.local>
2024-09-22 15:05:33 +0400

    Initial commit

---
+++ src/main/kotlin/com/example/credit_card_request_service/CreditCardRequestServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.credit_card_request_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditCardRequestServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditCardRequestServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/credit_card_request_service/CreditCardRequestServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.credit_card_request_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditCardRequestServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
