a27bc1f2ccff3aaa73aa93f99b265359c5b651d9
Gauri Manoj Bhutkar <g.bhutkar@ALHQLAP-794.local>
2024-09-28 18:20:35 +0400

    feat: add employment record details class

---
+++ src/main/kotlin/com/bank/scoreVerification/employmentVerification/domain/EmploymentRecord.kt
@@ -0,0 +1,32 @@
+package com.bank.scoreVerification.employmentVerification.domain
+
+import com.bank.creditcardrequest.domain.VerificationStatus
+import java.util.UUID
+
+class EmploymentRecord(
+    val id: UUID,
+    val employerDetails: EmployerDetails,
+    val jobTitle: String,
+    val position: String,
+    val employmentStatus: String,
+    val incomeInformation: IncomeInformation,
+    val employmentType: EmploymentType,
+    val verificationMethod: String,
+    val verificationStatus: VerificationStatus,
+    val verificationScore: Int
+)
+
+class EmployerDetails(
+    val employerName: String,
+    val employerContact: String,
+    val employerAddress: String,
+)
+
+class IncomeInformation(
+    val amount: Double,
+    val frequency: String,
+)
+
+enum class EmploymentType {
+    PERMANENT, PROBATIONARY, CONTRACTUAL;
+}
